3f13b60cd14b12ede3cb63b318c0808bba3a54e1
venhrun-petro <p.venhrun@pozitiff.com>
2018-12-06 14:53:21 +0200

    'my-test'

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@
 
 importScripts('https://daisy-pwa.netlify.com/sw-toolbox.js');
 
-toolbox.precache(["index.html","compiled.min.css"]);
+toolbox.precache(["https://daisy-pwa.netlify.com/index.html","https://daisy-pwa.netlify.com/compiled.min.css"]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('https://daisy-pwa.netlify.com/images/*', toolbox.cacheFirst);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
